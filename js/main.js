0ba16f0de73ff421402c93cddde093e2bdbf680d
Alix Richards <alix.richards@thisisdare.com>
2014-07-21 15:39:52 +0100

    created expandable section, added some images

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
+	$("#readMore1").click(function(){
+		$('#more1').slideToggle(500);
+	});
+
+	$("#readMore2").click(function(){
+		$('#more2').slideToggle(500);
+	});
+
+	$("#readMore3").click(function(){
+		$('#more3').slideToggle(500);
+	});
+
+	/*$('#readMore1').click(function(){
+		$('.more').toggle(function(){
+			$(this).next('div').slideToggle(500);
+			$(this).addClass('open');
+		},
+		function(){
+					$(this).next('div').slideToggle(500);
+					$(this).removeClass('open');
+				}	
+		);
+	}); */ 
+
+
 
 });
+
+
